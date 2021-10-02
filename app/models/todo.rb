036173e3bf6bb01f555125433bd46c80e4b81751
kcthanu <mcthanu@gmail.com>
2021-10-02 17:51:48 +0000

    One todo listed

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,5 +1,8 @@
-
-
 class Todo < ActiveRecord::Base
-  
+    
+  def to_pleasant_string
+    display_status = completed ? "[X]" : "[ ]"
+    "#{id}. #{due_date.to_s(:long)} #{todo_text} #{display_status}"
+  end  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  
   get "todos", to: "todos#index"
+  get "todos/:id", to: "todos#show"
+
 end
