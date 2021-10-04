13ded9f5b2591a626fb0e203b63be1cfcb4fb009
kcthanu <40052262+kcthanu@users.noreply.github.com>
2021-10-04 23:13:48 +0530

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     name = params[:name]
     email = params[:email]
     password = params[:password]
-    new_user = Todo.create!(name: name, email: email, password: password)
+    new_user = User.create!(name: name, email: email, password: password)
     
     response_text = "Your new user is created with the id #{new_user.id}"
     render plain: response_text
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
   def login
     email = params[:email]
     password = params[:password]
-    text = User.take("email = ? and password > ?", email, password) == nil ? false : true
+    text = User.where("email = ? and password > ?", email, password).first == nil ? false : true
     
     render plain: "#{text}"
   end
