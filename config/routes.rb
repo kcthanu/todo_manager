88a38d63d33f651b8372ad19827d871a06eeac7b
kcthanu <40052262+kcthanu@users.noreply.github.com>
2021-10-05 22:13:21 +0530

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   
   #get "todos", to: "todos#index"
-  #post "todos/create", to: todos#create"
+  #post "todos/create", to: "todos#create"
   #get "todos/:id", to: "todos#show"
 
   resources :todos
   resources :users
   
-  post "users/login", to: users#login
+  post "users/login", to: "users#login"
   
 end
